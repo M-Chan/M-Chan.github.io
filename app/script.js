7c11dcce912d02aa95963b2b74b5dff140ac570c
M-Chan <mantingchan2@gmail.com>
2022-08-20 13:14:29 +0100

    added content to projects page from JSON file

--- app/script.js
+++ app/script.js
@@ -5,6 +5,16 @@ let projectImages = [];
 let projectDates_Types = [];
 let projectDescriptions = [];
 
+const title = document.querySelector(".projectTitle");
+const image = document.querySelector("#image");
+const shortDes = document.querySelector(".shortDescription");       
+const description = document.querySelector("#projectDescription");  
+
+let buttonContainer = document.querySelector("#buttonsContainer");
+let childButton = document.createElement("div");
+childButton.classList.add("positionButton");
+const cloneButton = childButton.cloneNode(true);
+
 
 // fetchign data from JSON file and using the data
 fetch('./../app/data.json')
@@ -18,20 +28,26 @@ fetch('./../app/data.json')
     totalProjects = data.length; // defines 'totalProjetcss' from no. of elements in the JSON file
     // console.log("You have", totalProjects, "projects");
 
-      
-    // sets the first project...
-    // CODE HERE
-    
     // adds values to the arrays 'images' and 'description' using data from the fetched JSON file
     for (var i = 0; i < data.length; i++) {
       projectNames.push(data[i].name);
       projectImages.push(data[i].image);
       projectDates_Types.push(data[i].date_type);
       projectDescriptions.push(data[i].description);
-    }
 
-    console.log("project names are", projectNames);
-    console.log("project images are", projectImages);
-    console.log("project dates and types are", projectDates_Types);
-    console.log("project descriptions are", projectDescriptions);
+      console.log(buttonContainer);
+      console.log(cloneButton);
+      buttonContainer.appendChild(childButton.cloneNode(true));
+    }
+    // console.log("project names are", projectNames);
+    // console.log("project images are", projectImages);
+    // console.log("project dates and types are", projectDates_Types);
+    // console.log("project descriptions are", projectDescriptions);
+
+    // sets the first project...
+    title.innerHTML = projectNames[0];
+    image.setAttribute('style', 'background-image: url(' + projectImages[0] + ');');
+    shortDes.innerHTML = projectDates_Types[0];
+    // description.innerHTML = projectDescriptions[0]; 
+    console.log(title, shortDes, image, description);
 })
